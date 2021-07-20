f578697680549e536dd1391fce2c65477d0e5310
HexCorpDrone <hexcorpdrone@gmail.com>
2021-07-20 19:15:39 +0100

    Added music loading from mus directory

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,2 +1,32 @@
 extends Node
 
+var game_node: Node = get_parent()
+var songs: Array = []
+var song_index: int = 0
+
+func _ready():
+	# Load all available songs in the mus directory to the songs array.
+	print("Loading music.")
+	var dir: Directory = Directory.new()
+	if dir.open("res://mus") == OK:
+		print("Music directory opened.")
+		dir.list_dir_begin() # Inits the file reader stream.
+		var file_name: String = dir.get_next()
+		while file_name != "":
+			if file_name.get_extension() == "wav":
+				var audio_stream: AudioStream = load(file_name)
+				songs.append(audio_stream)
+				print("Loaded " + file_name)
+			file_name = dir.get_next()
+		print("Finished loading songs.")
+		dir.list_dir_end() # Closes file reader stream.
+	else:
+		print("Failed to open music directory.")
+		
+
+func change_music():
+	print("Changing music.")
+	if song_index == len(song_index):
+		song_index = 0
+	self.stream = songs[song_index]
+	self.playing = true
