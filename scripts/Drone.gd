89641208866783b17a259cbee640a38b74164b76
HexCorpDrone <hexcorpdrone@gmail.com>
2021-07-29 16:18:52 +0100

    Drone can now move

--- scripts/Drone.gd
+++ scripts/Drone.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends KinematicBody
 
 onready var icon_display: Sprite3D = get_node("Display/Icon")
 onready var id_display: Spatial = get_node("Display/ID")
@@ -6,6 +6,8 @@ onready var display_container: Spatial = get_node("Display")
 onready var sprite: AnimatedSprite3D = get_node("Body")
 var id: int = 0000
 var headbob_offset: Vector2 = Vector2(2.0, 1.9) #y if head is dipped, else x.
+var velocity: Vector3 = Vector3(0,0,0)
+var speed: float = 5
 
 func _ready():
 	set_id(5890)
@@ -30,8 +32,24 @@ func toggle_id():
 
 func toggle_display():
 	display_container.visible = !display_container.visible
-		
+
 func _process(delta):
+	
+	velocity = Vector3(0,0,0)
+	
+	# Get inputs, set velocity
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -1 * speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x = 1 * speed
+	if Input.is_action_pressed("move_up"):
+		velocity.z = -1 * speed
+	if Input.is_action_pressed("move_down"):
+		velocity.z = 1 * speed
+		
+	move_and_slide(velocity)
+	
+	# Calculate headbob
 	if sprite.frame % 2 != 0:
 		display_container.translation.y = headbob_offset.y
 	else:
