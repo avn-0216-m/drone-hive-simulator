3bc01c42be4256eeb355866b95b093f0de0938be
HexCorpDrone <hexcorpdrone@gmail.com>
2021-08-15 14:34:16 +0100

    Fixed instanced floor tile mesh + uvs

--- scripts/Multimeshes.gd
+++ scripts/Multimeshes.gd
@@ -8,7 +8,8 @@ onready var extern_corners = get_node("ExternalCorners")
 var floor_positions: Array = []
 
 func add_floor(pos: Vector3):
-	floors.multimesh.instance_count += 1
+	# changing the instance count resets all instance positions,
+	# so append them here and init them in the init func.
 	floor_positions.append(pos)
 	
 func add_wall(pos):
@@ -21,5 +22,6 @@ func add_extern_corner(pos):
 	return
 
 func init_multimeshes():
+	floors.multimesh.instance_count = len(floor_positions)
 	for i in range(len(floor_positions)):
 		floors.multimesh.set_instance_transform(i, Transform(Basis(), floor_positions[i]))
