5f95d9f3f08f0271d748241d8378882b0c1cc84a
HexCorpDrone <hexcorpdrone@gmail.com>
2021-10-10 22:39:31 +0100

    Added pet collar

--- scripts/Collar.gd
+++ scripts/Collar.gd
@@ -1,9 +1,9 @@
 extends Pickup
-class_name Flower
 
-func gift(object):
-	print("you have given a lovely flower to a friend :)")
-	object.heart.visible = true
+func own(object):
+	print("you now have a cute puppy!")
+	object.pet.visible = true
+	object.collar.visible = true
 	object.lena.visible = false
 	object.interactable = false
 	return true
@@ -11,7 +11,7 @@ func gift(object):
 func _ready():
 	icon_scale = Vector3(1.5,1.5,0)
 	icon_translation = Vector3(0,0,0.01)
-	interactions = {"Friend": funcref(self, "gift")}
+	interactions = {"Friend": funcref(self, "own")}
 
 func get_class() -> String:
-	return "Flower"
+	return "Collar"
--- scripts/Friend.gd
+++ scripts/Friend.gd
@@ -3,6 +3,8 @@ class_name Friend
 
 onready var heart = get_node("Heart")
 onready var lena = get_node("Lena")
+onready var collar = get_node("Collar")
+onready var pet = get_node("Pet")
 
 func get_class() -> String:
 	return "Friend"
