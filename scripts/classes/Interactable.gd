4624700a4aa73d4f35fbe45e2b32dc5610bca74c
HexCorpDrone <hexcorpdrone@gmail.com>
2021-11-02 23:34:04 +0000

    added todo reminder to interactable script

--- scripts/classes/Interactable.gd
+++ scripts/classes/Interactable.gd
@@ -5,6 +5,7 @@ var interactable: bool = true # If the object can be interacted with.
 var cursor_offset = Vector3(0,1,0) # Where the cursor should reside relative to the object.
 enum Type {NONE, ITEMS, DIRECT, BOTH}
 var type = Type.BOTH
+# TODO: implement this ^
 
 func interact(interactor):
 	print(self.name + " was interacted with. This is the default interaction function.")
