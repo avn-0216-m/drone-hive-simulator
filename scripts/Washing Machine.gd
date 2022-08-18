634574cf119d371ffc3b7110717c9c2eafeaf698
avn-0216-m <maidenofthebirdeternal@gmail.com>
2022-08-18 22:44:15 +0100

    updated washing machine texture

--- scripts/Washing Machine.gd
+++ scripts/Washing Machine.gd
@@ -2,3 +2,7 @@ extends Interactable
 
 func _ready():
 	$AnimationPlayer.play("New Anim")
+
+func interact(interactor):
+	UI.log("Spinning around and around...")
+	UI.log("Don't watch it too closely!")
