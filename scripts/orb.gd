2415145c72b9a1f8c8d8b24edf9f818b90493724
avn-0216-m <maidenofthebirdeternal@gmail.com>
2022-09-05 19:19:19 +0100

    Background shader texture ratio

--- scripts/orb.gd
+++ scripts/orb.gd
@@ -1,9 +1,1 @@
 extends Spatial
-
-func _ready():
-	
-	$TweenUVX.interpolate_property($MeshInstance.material_override, "uv1_offset:x", 0, 1, 60)
-	$TweenUVX.start()
-	
-	$TweenUVY.interpolate_property($MeshInstance.material_override, "uv1_offset:y", 0, 1, 120)
-	$TweenUVY.start()
