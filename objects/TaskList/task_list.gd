e4b81709a44e825150611a956f2db80288ea673a
avn-0216-m <maidenofthebirdeternal@gmail.com>
2025-05-28 19:31:45 +0100

    fancy text interp

--- objects/TaskList/task_list.gd
+++ objects/TaskList/task_list.gd
@@ -8,18 +8,29 @@ func _process(delta):
 	if Input.is_action_just_pressed("task_list"):
 		print("whee")
 		$AnimationPlayer.play("Up")
-		$RapidDelete.stop()
+		for node in $Control/TextureRect/VBoxContainer.get_children():
+			node.queue_free()
 	elif Input.is_action_just_released("task_list"):
 		$AnimationPlayer.play_backwards("Up")
-		$RapidDelete.start(0.1)
 		tasks = []
 
 func show_tasks(found_tasks: Array):
 	print("showin tasks")
 	tasks = found_tasks
-	$PopTimer.start()
+	$PopTimer.start(1)
 
 func pop_task():
+	# First check to see if there are any nearby tasks, if not, print a fallback.
+	if len(tasks) == 0 and $Control/TextureRect/VBoxContainer.get_child_count() == 0:
+		var list_item = list_item_src.instantiate()
+		print("nothing here fallback")
+		print(tasks)
+		list_item.text = "Nothing here..."
+		$Control/TextureRect/VBoxContainer.add_child(list_item)
+		$PopTimer.stop()
+		return
+	
+	# Then start popping tasks as normal.
 	var task_obj = tasks.pop_back()
 	if task_obj == null: return
 	var list_item = list_item_src.instantiate()
@@ -27,10 +38,3 @@ func pop_task():
 	if task_obj.task.task_complete:
 		list_item.text = "[s]" + list_item.text + "[/s]"
 	$Control/TextureRect/VBoxContainer.add_child(list_item)
-	
-func delete_line():
-	var line = $Control/TextureRect/VBoxContainer.get_child(0)
-	if line == null:
-		$RapidDelete.stop()
-	else:
-		$Control/TextureRect/VBoxContainer.get_child(0).queue_free()
--- objects/TaskList/task_list_item.gd
+++ objects/TaskList/task_list_item.gd
@@ -1,1 +1,5 @@
 extends RichTextLabel
+
+func _ready():
+	var tween = create_tween()
+	tween.tween_property(self, "visible_ratio", 1.0, 0.5)
